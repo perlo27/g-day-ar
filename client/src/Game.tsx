0cb8b945c6faeab5c02577af47a92d88009af9ca
artempelenev <pstemka@gmail.com>
2020-01-23 14:36:07 +0100

    add pins

--- client/src/Game.tsx
+++ client/src/Game.tsx
@@ -1,23 +1,35 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import {
   ViroARScene,
   ViroAmbientLight,
   ViroBox,
   ViroMaterials,
+  Viro3DObject,
   ViroSphere,
   ViroText,
-  Viro3DObject
 } from 'react-viro';
 import useWSServer from './useWSServer';
-import {StyleSheet} from 'react-native';
-import {getDeviceDetails} from './WebSocketController';
 
-const initialPosition = [Math.random() * 0.1, -1.5, -2];
+import { StyleSheet } from 'react-native';
+import { getDeviceDetails } from './WebSocketController';
 
+const pinConfig = [
+  [0, -1.9, -12],
+  [-0.5, -1.9, -13],
+  [0.5, -1.9, -13],
+  [-1, -1.9, -14],
+  [0, -1.9, -14],
+  [1, -1.9, -14],
+  [-1.5, -1.9, -15],
+  [-0.5, -1.9, -15],
+  [0.5, -1.9, -15],
+  [1.5, -1.9, -15],
+];
 const Game: React.FC = () => {
-  const {sendMessage, gameState} = useWSServer();
+  // const { sendMessage, gameState } = useWSServer();
   const [playerName, setPlayerName] = useState('');
   const sphereInstance = useRef(null);
+
   useEffect(() => {
     getDeviceDetails().then(details => setPlayerName(details.deviceName));
   }, []);
@@ -25,158 +37,77 @@ const Game: React.FC = () => {
   return (
     <ViroARScene>
       <ViroAmbientLight color="#aaaaaa" />
-
-      {gameState?.started && ( // scores
-        <>
-          <ViroText
-            width={1}
-            text="Score:"
-            position={[-1, 0.3, -3]}
-            style={styles.text}
-          />
-          {Object.entries(gameState?.players || {}).map(([key, value], index) => {
-            const {score, done, deviceName} = value;
-            return (
-              <ViroText
-                key={index}
-                width={1}
-                text={`${deviceName || key}: ${score}`}
-                // scale={[0.5, 0.5, 0.5]}
-                position={[-1, -0.5 * index, -3]}
-                style={[styles.text, {color: done ? 'red' : 'white'}]}
-              />
-            );
-          })}
-        </>
-      )}
-
-      {!gameState?.started && !gameState?.players[playerName] && (
-        <ViroText
-          text="Connect"
-          // scale={[0.5, 0.5, 0.5]}
-          position={[0, 0, -1]}
-          style={styles.text}
-          onClick={() => {
-            sendMessage({
-              connect: true,
-              deviceName: playerName,
-            });
-          }}
-        />
-      )}
-
-      {!gameState?.started && !!Object.keys(gameState?.players || {}).length && (
-        <ViroText
-          text="Start game"
-          position={[0, 0, -1]}
-          style={styles.text}
-          onClick={() => {
-            sendMessage({
-              gameStarted: true,
-            });
-          }}
-        />
-      )}
-
-      {gameState?.done && (
-        <ViroText
-          text="Restart"
-          position={[0, 0.2, -1]}
-          style={styles.text}
-          onClick={() => {
-            sendMessage({
-              restart: true,
-            });
-          }}
-        />
-      )}
-      {/* <ViroBox
-        // dragType="FixedToPlane"
-        // dragPlane={{
-        //   planePoint: [0, -1.5, 0],
-        //   planeNormal: [0, 1, 0],
-        //   maxDistance: 10,
-        // }}
-        onCollision={() => {
-          sendMessage({
-            player: playerName,
-            hit: true,
-          });
-        }}
-        onDrag={() => {}}
-        height={0.1}
-        length={0.3}
-        width={0.3}
-        position={initialPosition}
-        materials={['green']}
-        physicsBody={{
-          type: 'Static',
-          restitution: 1,
-          shape: {
-            type: 'Sphere',
-            params: [0.3],
-          },
-        }}
-      /> */}
-
       <ViroBox
         viroTag="ground"
         onCollision={() => {
-          sendMessage({
-            player: playerName,
-            done: true,
-          });
+          // sendMessage({
+          //   player: playerName,
+          //   done: true,
+          // });
         }}
-        height={0.5}
-        length={15}
-        width={4}
-        position={[0, -2, -9]}
+        height={0.1}
+        length={16}
+        width={5}
+        position={[0, -2.1, -8]}
         materials={['black']}
         physicsBody={{
           type: 'Static',
           restitution: 0.5,
         }}
       />
-      {/* {gameState?.started && !gameState?.done && ( */}
-        <ViroSphere
-          viroTag="ball"
-          dragType="FixedToPlane"
-          dragPlane={{
-            planePoint: [0, -1.5, 0],
-            planeNormal: [0, 1, 0],
-            maxDistance: 5,
+      <ViroSphere
+        viroTag="ball"
+        dragType="FixedToPlane"
+        dragPlane={{
+          planePoint: [0, -1.5, 0],
+          planeNormal: [0, 1, 0],
+          maxDistance: 5,
+        }}
+        onDrag={() => {}}
+        onFuse={() => {
+          console.log('onFuse');
+        }}
+        onHover={() => {
+          console.log('onFuse');
+        }}
+        radius={0.3}
+        ref={sphereInstance}
+        position={[0, -1.5, -2]}
+        materials={['red']}
+        physicsBody={{
+          type: 'Dynamic',
+          mass: 6,
+          restitution: 1,
+        }}
+      />
+      {pinConfig.map((position, index) => (
+        <Viro3DObject
+          key={index}
+          // ref={a[index]}
+          source={require('./res/pin.obj')}
+          position={position}
+          resources={[
+            require('./res/pinRessource.mtl'),
+            require('./res/pinTexture.jpg'),
+          ]}
+          onTransformUpdate={() => {
+            console.log('---> qweqwe ');
           }}
-          onDrag={() => {}}
-          onFuse={() => { console.log('onFuse')}}
-          onHover={() => { console.log('onFuse') }}
-          radius={0.20}
-          ref={sphereInstance}
-          position={[0, -1.5, -2]}
-          materials={['red']}
+          scale={[0.04, 0.04, 0.04]}
+          rotation={[-90, 0, 0]}
+          materials={['pin']}
+          type="OBJ"
           physicsBody={{
             type: 'Dynamic',
-            mass: 6,
-            restitution: 1,
+            mass: 1.5,
+            restitution: 0.5,
+            shape: {
+              type: 'Compound',
+              params: [0.3, 1.5, 0.3],
+            },
           }}
         />
-        <Viro3DObject source={require('./res/pin.obj')}
-              position={[0, -1.7, -15]}
-              resources={[require('./res/pinRessource.mtl'), require('./res/pinTexture.jpg')]}
-              scale={[0.04,0.04,0.04]}
-              rotation={[-90,0,0]}
-              materials={["pin"]}
-              type="OBJ" 
-              physicsBody={{
-                type: 'Dynamic',
-                mass: 1.5,
-                restitution: 1,
-                shape: {
-                  type: 'Compound',
-                  params: [0.3,2,0.3],
-                },
-              }}
-          />
-      {/* )} */}
+      ))}
     </ViroARScene>
   );
 };
@@ -186,7 +117,7 @@ ViroMaterials.createMaterials({
     diffuseTexture: require('./res/grid_bg.jpg'),
     diffuseColor: 'green',
   },
-  blue: {diffuseTexture: require('./res/grid_bg.jpg'), diffuseColor: 'blue'},
+  blue: { diffuseTexture: require('./res/grid_bg.jpg'), diffuseColor: 'blue' },
   red: {
     diffuseTexture: require('./res/face.jpg'),
     // diffuseColor: 'red',
@@ -197,7 +128,12 @@ ViroMaterials.createMaterials({
   },
   pin: {
     diffuseTexture: require('./res/pinTexture.jpg'),
-    // diffuseColor: 'green',
+  },
+  white: {
+    lightingModel: 'PBR',
+    diffuseTexture: require('./res/tesla/object_car_main_Base_Color.png'),
+    metalnessTexture: require('./res/tesla/object_car_main_Metallic.png'),
+    roughnessTexture: require('./res/tesla/object_car_main_Roughness.png'),
   },
 });
 
